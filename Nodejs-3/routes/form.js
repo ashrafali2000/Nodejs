e5c24dc378e428932b8f53fc90aad6c0382f3db4
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-24 13:42:40 +0500

    create node-3 code

---
+++ Nodejs-3/routes/form.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const fileHtmlPath = path.join(process.cwd(), "views", "form.html")
+
+const router = express.Router();
+// /form/
+router.get("/", (req, res) => {
+res.sendFile(fileHtmlPath);
+})
+
+router.post("/submit", (req, res) => {
+    res.send(req.body);
+    })
+
+module.exports = router;
