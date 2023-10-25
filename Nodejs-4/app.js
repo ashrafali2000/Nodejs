61e67a99f32084738548144190de15b2dafd1f65
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-25 16:45:06 +0500

    paste all Nodejs-3 file

---
+++ Nodejs-4/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+const form = require("./routes/form")
+const todo = require("./routes/todo")
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+app.use(express.static(path.join(process.cwd(), "public")))
+
+// As a localStorage
+app.set("view engine", "ejs");
+app.set("views", "views")
+
+app.use((req, res, next) => {  //Auth midleware
+    req.data = "Ashraf";
+    next();
+})
+
+app.use("/todo", todo);
+app.use("/form", form);
+
+app.listen(3000);
