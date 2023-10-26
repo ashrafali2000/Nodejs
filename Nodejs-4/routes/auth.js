682089adb960bb9ffbf890ca4240d2711881155f
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-26 11:21:19 +0500

    create signin signup route in auth

--- Nodejs-4/routes/auth.js
+++ Nodejs-4/routes/auth.js
@@ -1,19 +1,23 @@
 const express = require("express");
-const { createUser } = require("../controllers/auth");
+const { createUser, login } = require("../controllers/auth");
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-    
-})
+router.post("/login", async (req, res) => {
+  try {
+    const response = await login(req.body.email, req.body.password);
+    res.status(200).send(response);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-router.post("/signup", async(req, res) => {
-    try{
-        await createUser(req.body.email, req.body.password);
-        res.status(200).send("user sucessfully created")
-    }catch(err){
-res.send(err)
-    }
-})
+router.post("/signup", async (req, res) => {
+  try {
+    const response = await createUser(req.body.email, req.body.password);
+    res.status(200).send(response);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
