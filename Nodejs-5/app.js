6bfbd7fc95237456bce1a97bba2707639eeaca2f
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-26 16:26:25 +0500

    require cors

--- Nodejs-5/app.js
+++ Nodejs-5/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const cors = require('cors')
 const app = express();
 
-const form = require("./routes/form")
-const todo = require("./routes/todo")
+const form = require("./routes/form");
+const todo = require("./routes/todo");
 const auth = require("./routes/auth");
+const products = require("./routes/products");
 const { varify } = require("./middlewares/auth");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
+app.use(cors())
 app.use(express.static(path.join(process.cwd(), "public")))
 
 // As a localStorage
@@ -24,6 +26,7 @@ app.use((req, res, next) => {  //Auth midleware
 
 app.use("/todo", varify, todo);
 app.use("/form", form);
-app.use("/auth", auth);
+app.use("/signup/auth", auth);
+app.use("/products", products);
 
-app.listen(3000);
+app.listen(8000);
