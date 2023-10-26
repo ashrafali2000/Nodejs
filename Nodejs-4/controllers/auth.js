bee0e46f0ee8a2eb24f3215ede0e29f52bfcffdc
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-26 11:23:05 +0500

    login with bcryptcompare controller

--- Nodejs-4/controllers/auth.js
+++ Nodejs-4/controllers/auth.js
@@ -1,9 +1,22 @@
-const { createUser } = require("../models/user");
-
+const { createUser, findUser } = require("../models/user");
+const bcrypt = require('bcrypt');
 exports.createUser = async (email, password) => {
   try {
     const userId = Date.now();
-    await createUser(email, password, userId);
+    const hashPassword = await bcrypt.hash(password, 12)
+    return await createUser(email, hashPassword, userId);
+  } catch (err) {
+    throw err;
+  }
+};
+exports.login = async (email, password) => {
+  try {
+    const user = await findUser(email);
+    const result =  await bcrypt.compare(password, !!user && user.password);
+    if(result){
+        return "login sucessfull"
+    }
+    return "wrong email id or Password"
   } catch (err) {
     throw err;
   }
